f263afce0cad010bd1542dcf9fa54b85ee167ffa
hanyong <313982441@qq.com>
2016-12-25 11:20:28 +0800

    hello servlet

---
+++ src/main/java/com/example/javawebdev/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.example.javawebdev;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		String name = req.getParameter("name");
+		out.printf("Hello %s!\n", name);
+	}
+	
+}
