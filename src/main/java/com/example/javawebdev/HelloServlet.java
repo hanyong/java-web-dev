0f9b05228186244d3a8483c1fcc48f6490ac5c43
hanyong <313982441@qq.com>
2017-12-09 23:43:03 +0800

    简单 HelloServlet 示例

---
+++ src/main/java/com/example/javawebdev/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.example.javawebdev;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		resp.setContentType("text/plain");
+		PrintWriter out = resp.getWriter();
+		out.printf("Hello Servlet %s\n", name);
+	}
+	
+}
