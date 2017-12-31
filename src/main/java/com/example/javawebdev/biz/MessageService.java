4eb3afa753ce2568f2d42aa06eab7351aee2e8cd
hanyong <313982441@qq.com>
2017-12-31 19:55:04 +0800

    添加 yaml 配置文件

--- src/main/java/com/example/javawebdev/biz/MessageService.java
+++ src/main/java/com/example/javawebdev/biz/MessageService.java
@@ -1,9 +1,16 @@
 package com.example.javawebdev.biz;
 
+@lombok.Getter
+@lombok.Setter
+@lombok.ToString
 public class MessageService {
 
+	protected String prefix;
+	
 	public String getMessage(String key) {
-		return System.getProperty(key);
+		String value = System.getProperty(key);
+		value = prefix + " " + value;
+		return value;
 	}
 	
 }
