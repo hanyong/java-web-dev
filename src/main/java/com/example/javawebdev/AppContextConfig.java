8bece6f65bcf5ddd00bea6b94e9defc5b3120492
hanyong <313982441@qq.com>
2018-01-06 16:38:09 +0800

    使用 spring boot 启动应用

--- src/main/java/com/example/javawebdev/AppContextConfig.java
+++ src/main/java/com/example/javawebdev/AppContextConfig.java
@@ -1,5 +1,7 @@
 package com.example.javawebdev;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +14,17 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 	org.springframework.web.servlet.handler.SimpleServletHandlerAdapter.class,
 	org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter.class,
 })
+@ImportAutoConfiguration({
+	org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration.class,
+	org.springframework.boot.autoconfigure.web.ServerPropertiesAutoConfiguration.class,
+	org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration.class,
+})
 public class AppContextConfig {
 
+	public static void main(String[] args) {
+		SpringApplication.run(AppContextConfig.class, args);
+	}
+	
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
