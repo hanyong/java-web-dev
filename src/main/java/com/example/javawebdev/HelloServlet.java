a3088c7b0ffd9a33905853b5f56318886b7fb239
hanyong <313982441@qq.com>
2018-01-06 16:36:26 +0800

    fix bug, spring 中 HttpServletBean 必须显式声明执行 initServletBean() 初始化

--- src/main/java/com/example/javawebdev/HelloServlet.java
+++ src/main/java/com/example/javawebdev/HelloServlet.java
@@ -3,10 +3,13 @@ package com.example.javawebdev;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.HttpServletBean;
@@ -17,10 +20,18 @@ import com.example.javawebdev.biz.MessageService;
 public class HelloServlet extends HttpServletBean {
 
 	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 	
 	@Autowired
 	protected MessageService messageService;
 
+	@PostConstruct
+	@Override
+	protected void initServletBean() throws ServletException {
+		super.initServletBean();
+		logger.info("HelloServlet.initServletBean ...");
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String key = req.getParameter("key");
