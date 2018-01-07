ad3d7b58d08cabbb7fef429288410fa1d427a788
hanyong <313982441@qq.com>
2018-01-07 16:27:42 +0800

    显式配置 @EnableConfigurationProperties

--- src/main/java/com/example/javawebdev/AppContextConfig.java
+++ src/main/java/com/example/javawebdev/AppContextConfig.java
@@ -1,5 +1,6 @@
 package com.example.javawebdev;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 	org.springframework.web.servlet.handler.SimpleServletHandlerAdapter.class,
 	org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter.class,
 })
+@EnableConfigurationProperties
 public class AppContextConfig {
 
 	@Bean
